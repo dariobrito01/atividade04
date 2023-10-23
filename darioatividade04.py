f02ccc3643e74999ad67efd848b2093f1d4b38be
Dário <147437499+dariobrito01@users.noreply.github.com>
2023-10-23 20:11:57 +0300

    Add files via upload

---
+++ darioatividade04.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+print("Descubra o sucessor e o antecessor de um número qualquer: ")
+número = int (input('digite um número'))
+sucessor = (número+1)
+antecessor = (número-1)
+print(f'o sucessor do seu número é {sucessor} e o antecessor é {antecessor}')
